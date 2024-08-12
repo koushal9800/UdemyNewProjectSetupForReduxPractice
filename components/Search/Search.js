2d0ea9d3b155f4062d8e7bc6d88ee15e26fc25b1
koushal.barick <koushal.barick@stockedge.com>
2024-08-12 12:54:22 +0530

    search is ok

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -3,53 +3,53 @@ import { View, Text, TextInput, Pressable } from "react-native";
 import style from "./style";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
+const Search = ({ onSearch, placeholder }) => {
+  const handleFocus = () => {
+    textInputRef.current.focus();
+  };
 
-const Search = (props) => {
+  const textInputRef = useRef(null);
+  const [search, setSearch] = useState("");
 
-    const habdleFocus = () => {
-        textInputRef.current.focus()
-    }
+  const handleSearch = (searchValue) => {
+    setSearch(searchValue);
+    onSearch(searchValue);
+  };
 
-    const textInputRef = useRef(null)
-    const [search, setSearch] = useState('')
-
-
-    const handleSearch = searchValue => {
-        setSearch(searchValue)
-        props.onSearch(searchValue)
-    }
-    return (
-        <Pressable style={{
-            flexDirection: 'row', paddingHorizontal: 6,
-            backgroundColor: '#F3F5F9',
-            height: 50,
-            alignItems: 'center',
-            borderRadius: 15
-        }}
-            onPress={habdleFocus}
-        >
-            <FontAwesomeIcon icon={faSearch} color={'#25C0FF'} size={22} />
-
-            <TextInput ref={textInputRef} style={style.searchInput}
-                value={search}
-                onChangeText={(value) => handleSearch(value)}
-                placeholder='Search'
-                
-            />
-        </Pressable>
-    )
-}
-
-export default Search
-
-Search.default = {
-    onSearch: () => { },
-    placeholder: 'Search'
+  return (
+    <Pressable
+      style={{
+        flexDirection: "row",
+        paddingHorizontal: 6,
+        backgroundColor: "#F3F5F9",
+        height: 50,
+        alignItems: "center",
+        borderRadius: 15,
+      }}
+      onPress={handleFocus}
+    >
+      <FontAwesomeIcon icon={faSearch} color={"#25C0FF"} size={22} />
+      <TextInput
+        ref={textInputRef}
+        style={style.searchInput}
+        value={search}
+        onChangeText={(value) => handleSearch(value)}
+        placeholder={placeholder}
+      />
+    </Pressable>
+  );
 };
 
-Search.PropTypes = {
-    onSearch: PropTypes.func,
-    placeholder: PropTypes.string
-}
+Search.defaultProps = {
+  onSearch: () => {},
+  placeholder: "Search",
+};
+
+Search.propTypes = {
+  onSearch: PropTypes.func,
+  placeholder: PropTypes.string,
+};
+
+export default Search;
